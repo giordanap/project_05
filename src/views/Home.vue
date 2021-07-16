57613da00bf99a6fb03f3c9280e70e1e3fd5e3b9
Giordan <garredondop@uni.pe>
2021-07-16 00:39:28 -0500

    component_Input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,96 +5,8 @@
   <form @submit.prevent="processForm">
     
     <!-- in Bootstrap can use mt-5 mb-5 or my-5 -->
-    <h2 class="my-5" >Form with Vue.js</h2>
-
-    <input 
-      type="text"
-      class="form-control my-2"
-      placeholder="Input name"
-      v-model.trim="task.name"
-    >
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox" 
-        id="check-1 "
-        class="form-check-input"
-        v-model="task.categories"
-        value="javascript"
-      >
-      <label for="check-1" class="form-check-label"> Javascript </label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox" 
-        id="check-1 "
-        class="form-check-input"
-        v-model="task.categories"
-        value="node js"
-      >
-      <label for="check-1" class="form-check-label"> Node.js </label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox" 
-        id="check-1 "
-        class="form-check-input"
-        v-model="task.categories"
-        value="python"
-      >
-      <label for="check-1" class="form-check-label"> Python </label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox" 
-        id="check-1 "
-        class="form-check-input"
-        v-model="task.categories"
-        value="c#"
-      >
-      <label for="check-1" class="form-check-label"> C# </label>
-    </div>
-
-    <div class="mt-2">
-
-      <div class="form-check form-check-inline">
-        <input 
-          type="radio"
-          id="radio-1"
-          class="form-check-input"
-          v-model="task.state"
-          value="urgent"
-        >
-        <label for="radio-1" class="form-check-label"> Urgent </label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input 
-          type="radio"
-          id="radio-2"
-          class="form-check-input"
-          v-model="task.state"
-          value="relax"
-        >
-        <label for="radio-2" class="form-check-label"> Relax </label>
-      </div>
-
-    </div>
-
-    <div class="mt-2">
-      <input 
-        type="number"
-        class="form-control"
-        v-model.number ="task.number"
-      >
-    </div>
-
-    <button 
-      class="btn btn-dark btn-block mt-2" 
-      type="submit"
-      :disabled='lockButton'
-    >
-      Process
-    </button>
-
+    <h1 class="my-5" >Form with Vue.js</h1>
+    <Input :task="task"/>
   </form>
   <hr>
   <p>
@@ -103,12 +15,17 @@
 </template>
 
 <script>
+import Input from '../components/Input.vue'
 
 export default {
   name: 'Home',
+  components: {
+    Input
+  },
   data() {
     return {
       task: {
+        id: '',
         name: '',
         categories: [],
         state: '',
@@ -135,11 +52,6 @@ export default {
         state: '',
         number: 0
       }
-    }
-  },
-  computed: {
-    lockButton() {
-      return this.task.name.trim() === "" ? true : false
     }
   }
 }
